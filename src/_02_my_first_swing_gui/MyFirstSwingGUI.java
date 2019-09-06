78af316fef01515115f4901d934e2e70c1c38305
Airsoft102 <42328862+Airsoft102@users.noreply.github.com>
2019-09-05 19:44:39 -0700

    fodrt sr

--- src/_02_my_first_swing_gui/MyFirstSwingGUI.java
+++ src/_02_my_first_swing_gui/MyFirstSwingGUI.java
@@ -16,32 +16,34 @@ public class MyFirstSwingGUI {
 	public void run() {
 
 		// 1. Create and initialize an object of the JFrame class
-		JFrame j= new JFrame();
+		JFrame frame= new JFrame();
 		// 2. Set your JFrame object to be visible
-	j.setVisible(true);
+	frame.setVisible(true);
 		// 3. Run your program. Do you see your window? It's probably very
 		// small.
 
 		// 4. Set the default close operation to JFrame.EXIT_ON_CLOSE
-j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		// 5. Create and initialize an object of the JPanel class
-
+JPanel Panel=new JPanel();
 		// 6. Create and initialize an object of the JLabel class
-
+JLabel label=new JLabel();
 		// 7. Set the text of the JLabel to a lovely greeting.
-
+label.setText("hello welcome!");
 		// 8. Add the JPanel object to the JFrame
-
+frame.add(Panel);
 		// 9. Add the JLabel object to the JPanel
-
+Panel.add(label);
 		// 10. Pack your JFrame.
-
+frame.pack();
 		// 11. Run your program again. Do you see your message.
 
 		// 12. Use the loadImage method to set the icon of the JLabel object.
 
-		// 13. Re-pack the JFrame object.
+label.setIcon(loadImage());
 
+		// 13. Re-pack the JFrame object.
+frame.pack();
 		// 14. Run the program one more time. Do you see the image?
 
 	}
--- src/_03_photo_quiz/PhotoQuiz.java
+++ src/_03_photo_quiz/PhotoQuiz.java
@@ -25,22 +25,26 @@ public class PhotoQuiz {
 
 		// 1. find an image on the internet, and put its URL in a String
 		// variable (from your browser, right click on the image, and select
-		// “Copy Image Address”)
-
+		// â€œCopy Image Addressâ€�)
+String s="https://s7d5.scene7.com/is/image/horizonhobby/EFL15250_PP_Footer?$Default$";
 		// 2. create a variable of type "Component" that will hold your image
-
+Component c;
 		// 3. use the "createImage()" method below to initialize your Component
-
+c=createImage(s);
 		// 4. add the image to the quiz window
-
+quizWindow.add(c);
 		// 5. call the pack() method on the quiz window
-
+quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+String j=JOptionPane.showInputDialog("what type of plane is this?");
 		// 7. print "CORRECT" if the user gave the right answer
-
+if (j.equals("A Turbo Timber on floats.")) {
+	JOptionPane.showMessageDialog(null, "good job");
+} 
 		// 8. print "INCORRECT" if the answer is wrong
-
+else  {
+JOptionPane.showMessageDialog(null, "Nope, It is a Turbo Timber on floats.");
+}
 		// 9. remove the component from the quiz window (you may not see the
 		// effect of this until step 12)
 
