943cb42a8df5fd47595ad98a797d58ebe030aed1
League Student <league@iMac-21.attlocal.net>
2019-10-24 19:53:30 -0700

    rtyre
    gfdsg

--- src/_07_fortune_cookie/cookie.java
+++ src/_07_fortune_cookie/cookie.java
@@ -1,5 +1,19 @@
 package _07_fortune_cookie;
 
-public class cookie {
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class cookie {
+	  public void showButton() {
+          System.out.println("Button clicked");
+          JFrame frame=new JFrame();
+          frame.setVisible(true);
+          JButton button = new JButton();
+          frame.add(button);
+          frame.pack();
+          button.addActionListener((ActionListener) this);
 }
+}
+ 
--- src/_07_fortune_cookie/cookierunner.java
+++ src/_07_fortune_cookie/cookierunner.java
@@ -1,5 +1,7 @@
 package _07_fortune_cookie;
 
 public class cookierunner {
-
+	  public static void main(String[] args) {
+          new cookie().showButton();
+    }
 }
