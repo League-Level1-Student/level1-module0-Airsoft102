6c2fe5bfc972a91f7e6e9d2c3c0eb34262c994c8
League Student <league@iMac-23.attlocal.net>
2019-12-19 19:15:01 -0800

    ghjgk
    hczhgdxytgur f gf j fyt

--- src/_08_jack_in_the_box/jack.java
+++ src/_08_jack_in_the_box/jack.java
@@ -1,8 +1,10 @@
 package _08_jack_in_the_box;
 
 import java.awt.Button;
+import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 import javax.swing.Icon;
@@ -13,65 +15,92 @@ import javax.swing.JLabel;
 
 import _07_fortune_cookie.cookie;
 
-public class jack implements ActionListener {JButton  button= new JButton();
-int i=0;
-private void showPicture(String fileName) {
-    try {
-        JLabel imageLabel = createLabelImage(fileName);
-        JFrame frame = new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(imageLabel);
-        frame.setVisible(true);
-    } catch (Exception e) {
-        e.printStackTrace();
-    }
-}
-private JLabel createLabelImage(String fileName) {
-	try {
-            URL imageURL = getClass().getResource(fileName);
-            if (imageURL == null) {
-	       System.err.println("Could not find image " + fileName);
-	       return new JLabel();
-            } else {
-                Icon icon = new ImageIcon(imageURL);
-                JLabel imageLabel = new JLabel(icon);
-                return imageLabel;
-            }
-        } catch (Exception e) {
-            System.err.println("Could not find image " + fileName);
-            return new JLabel();
-        }
-}
-public static void main(String[] args) {
-	
-	 new jack().showButton();
-	
-	
-}
-	public void showButton() {
-        System.out.println("Button clicked");
-        JFrame frame=new JFrame();
-        frame.setVisible(true);
-        
-        frame.add(button);
-        frame.pack();
-        button.addActionListener(this);
-	
-		
-      
+public class jack implements ActionListener {
+	JButton button = new JButton();
+	int i = 0;
+
+	private void showPicture(String fileName) {
+		try {
+			JLabel imageLabel = createLabelImage(fileName);
+			JFrame frame = new JFrame();
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.add(imageLabel);
+			frame.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
+
+	private JLabel createLabelImage(String fileName) {
+		try {
+			URL imageURL = getClass().getResource(fileName);
+			if (imageURL == null) {
+				System.err.println("Could not find image " + fileName);
+				return new JLabel();
+			} else {
+				Icon icon = new ImageIcon(imageURL);
+				JLabel imageLabel = new JLabel(icon);
+				return imageLabel;
+			}
+		} catch (Exception e) {
+			System.err.println("Could not find image " + fileName);
+			return new JLabel();
+		}
+	}
+
+	public static void main(String[] args) {
+
+		new jack().showButton();
+
+	}
+
+	public void showButton() {
+		System.out.println("Button clicked");
+		JFrame frame = new JFrame();
+		frame.setVisible(true);
+
+		frame.add(button);
+		frame.pack();
+		button.addActionListener(this);
+
+		
+
+	}
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		
-			JButton buttonPressed = (JButton) e.getSource();
-	if (buttonPressed==button) {
-		i++;
-		if (i==5) {
-			showPicture("jackInTheBox.png");
-		}
-	}
-	
-}
-}
+i++;
+if (i==5) {
 
+	JFrame quizWindow = new JFrame();
+	quizWindow.setVisible(true);
+	// This will make sure the program exits when you close the window
+	quizWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	String s = "https://upload.wikimedia.org/wikipedia/en/thumb/6/6e/Jack_in_the_Box_2009_logo.svg/1200px-Jack_in_the_Box_2009_logo.svg.png";
+
+	Component c = null;
+
+	try {
+		c = createImage(s);
+	} catch (MalformedURLException e1) {
+		// TODO Auto-generated catch block
+		e1.printStackTrace();
+	}
+
+	quizWindow.add(c);
+
+	quizWindow.pack();
+
+
+}
+		}
+	
+
+	private Component createImage(String imageUrl) throws MalformedURLException {
+		URL url = new URL(imageUrl);
+		Icon icon = new ImageIcon(url);
+		JLabel imageLabel = new JLabel(icon);
+		return imageLabel;
+	}
+}
